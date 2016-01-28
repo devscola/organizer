47b08a22d9f82292d0be321b9d5638386e6d1bf4
David P <david@davidpena.es>
2016-01-28 21:30:57 +0100

    elements with child

--- app.rb
+++ app.rb
@@ -1,29 +1,38 @@
 class Element
   attr_accessor :title
   attr_accessor :description
+  attr_accessor :child
 
   def initialize(title)
     @title = title
-    @description = nil
   end
 end
 
 describe "Element" do
+  let (:title) { "El libro de Camilin" }
+
   it "have a title" do
-    title = "El libro de Camilin"
+    element = Element.new(:title)
 
-    element = Element.new(title)
-
-    expect(element.title).to eq(title)
+    expect(element.title).to eq(:title)
   end
 
-  it "have a description" do
-    title = "El libro de Camilin"
+  it "can have a description" do
     plot = "El mejor libro del mundo"
 
-    element = Element.new(title)
+    element = Element.new(:title)
     element.description= plot
 
     expect(element.description).to eq(plot)
+  end
+
+  it "can have a child" do
+    child_title = "Camilin audiobook"
+    child_element = Element.new(child_title)
+    element = Element.new(:title)
+
+    element.child = child_element
+
+    expect(element.child).to eq(child_element)
   end
 end
